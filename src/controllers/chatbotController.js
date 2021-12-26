4090cc201d3c083336352530fed1fd16d2353e86
Mint Nguyen <thanhnhanss0243@gmail.com>
2021-12-27 02:28:26 +0700

    update

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -132,7 +132,10 @@ function handlePostback(sender_psid, received_postback) {
         response = {
             "text": "Oops, try sending another image."
         }
-    }
+    } else if (payload === "GET_STARTED") {
+        response = {
+            "text": "Xin chào mừng bạn abc đến với trang tư vấn sức khoẻ của VLU"
+        }
     // Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
 }
