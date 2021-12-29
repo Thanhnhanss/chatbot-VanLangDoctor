6f05473e206e0d632d2fbf64a41a0072371c2c15
Mint Nguyen <thanhnhanss0243@gmail.com>
2021-12-30 02:10:24 +0700

    update

--- src/services/chatbotServices.js
+++ src/services/chatbotServices.js
@@ -1,78 +1,78 @@
 require("dotenv").config();
-import { response } from "express";
+import {
+    response
+} from "express";
 import request from "request";
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 let callSendAPI = (sender_psid, response) => {
-  let request_body = {
-    recipient: {
-      id: sender_psid,
-    },
-    message: response,
-  };
+    let request_body = {
+        recipient: {
+            id: sender_psid,
+        },
+        message: response,
+    };
 
-  // Send the HTTP request to the Messenger Platform
-  request(
-    {
-      uri: "https://graph.facebook.com/v9.0/me/messages",
-      qs: {
-        access_token: PAGE_ACCESS_TOKEN,
-      },
-      method: "POST",
-      json: request_body,
-    },
-    (err, res, body) => {
-      if (!err) {
-        console.log("message sent!");
-      } else {
-        console.error("Unable to send message:" + err);
-      }
-    }
-  );
+    // Send the HTTP request to the Messenger Platform
+    request({
+            uri: "https://graph.facebook.com/v9.0/me/messages",
+            qs: {
+                access_token: PAGE_ACCESS_TOKEN,
+            },
+            method: "POST",
+            json: request_body,
+        },
+        (err, res, body) => {
+            if (!err) {
+                console.log("message sent!");
+            } else {
+                console.error("Unable to send message:" + err);
+            }
+        }
+    );
 };
 
 let getUserName = () => {
-  let username = "";
-  // Send the HTTP request to the Messenger Platform
-  request(
-    {
-      uri: `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
-      qs: {
-        access_token: PAGE_ACCESS_TOKEN,
-      },
-      method: "GET",
-    },
-    (err, res, body) => {
-      console.log(body);
-      if (!err) {
-        response = JSON.parse(res);
-        // "first_name": "Peter",
-        // "last_name": "Chang",
-        username = `${response.first_name} ${response.last_name}`;
-      } else {
-        console.error("Unable to send message:" + err);
-      }
-    }
-  );
-  return username;
+    let username = "";
+    // Send the HTTP request to the Messenger Platform
+    request({
+            "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+            "qs": {
+                "access_token": PAGE_ACCESS_TOKEN
+            },
+            "method": "GET",
+        },
+        (err, res, body) => {
+            console.log(body);
+            if (!err) {
+                response = JSON.parse(res);
+                // "first_name": "Peter",
+                // "last_name": "Chang",
+                username = `${response.first_name} ${response.last_name}`;
+            } else {
+                console.error("Unable to send message:" + err);
+            }
+        }
+    );
+    return username;
 };
 
 let handleGetStarted = (sender_psid) => {
-  return new Promise(async (resolve, reject) => {
-    try {
-        let username = await getUserName(sender_psid);
-      let response = {
-        text: `Xin chào bạn ${username} đã đến với website Bác sĩ Văn Lang`,
-      };
-      await callSendAPI(sender_psid, response);
-      resolve("done");
-    } catch (e) {
-      reject(e);
-    }
-  });
+    return new Promise(async (resolve, reject) => {
+        try {
+            let username = await getUserName(sender_psid);
+            let response = {
+                text: `Xin chào bạn ${username} đã đến với website Bác sĩ Văn Lang`,
+            };
+            await callSendAPI(sender_psid, response);
+            resolve("done");
+        } catch (e) {
+            reject(e);
+        }
+    });
 };
 
 module.exports = {
-  handleGetStarted: handleGetStarted,
+    handleGetStarted: handleGetStarted,
 };
