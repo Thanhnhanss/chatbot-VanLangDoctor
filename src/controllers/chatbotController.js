ddba94e1a8306afccdc80ce89e3af0dcad016d44
Mint Nguyen <thanhnhanss0243@gmail.com>
2022-04-10 10:41:28 +0700

    feedback

--- src/controllers/chatbotController.js
+++ src/controllers/chatbotController.js
@@ -459,7 +459,7 @@ let handlePostBookTable = async (req, res) => {
 
 let handleFeedBack = (req, res) => {
     // console.log(data1);
-    return res.render('feedback.js');
+    return res.render('feedback.ejs');
 }
 let handlePostFeedback = async (req, res) => {
     console.log('Begin handle post feedback table');
